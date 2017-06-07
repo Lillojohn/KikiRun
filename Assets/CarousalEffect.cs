9dbf22ae3d5eeb771038775c790645a424a489f7
Jonathan Emanuels <j.emanuels@outlook.com>
2017-06-07 09:29:51 +0200

    Carousal

---
+++ Assets/CarousalEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarousalEffect : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate (Vector3.left * speed);
+		if (gameObject.transform.position.x < - 13) {
+			gameObject.transform.position = new Vector3 (12, -0.200822f, 0);
+		}
+	}
+}
