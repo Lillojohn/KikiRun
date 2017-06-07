ab9086e9bd7d890d40994bec82b87f4eee1eac25
Jonathan Emanuels <j.emanuels@outlook.com>
2017-06-07 10:05:07 +0200

    Geluid + 3 blokjes

---
+++ Assets/Audio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour {
+
+	public AudioClip impact;
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+		audio.PlayOneShot(impact, 0.7F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -23,13 +23,21 @@ public class BlockManager : MonoBehaviour {
 		timer++;
 
 		if (timer > triggerTime + randomOffset && _pauzeManager.GetComponent<PauzeManager>().GetPauze() == false) {
-			int random = Random.Range (0, 2);
+			int random = Random.Range (0, 4);
 			if (random == 0) {
 				CreateBlock ();
 			}
 
 			if (random == 1) {
 				CreateTwoBlocks ();
+			}
+
+			if (random == 2) {
+				CreateBlock ();
+			}
+
+			if (random == 3) {
+				CreateThreeBlocks ();
 			}
 
 			timer = 0;
@@ -44,6 +52,15 @@ public class BlockManager : MonoBehaviour {
 	public void CreateTwoBlocks(){
 		gameObjectList.Add(Instantiate(Resources.Load("Prefabs/BlockA")) as GameObject); 
 		gameObjectList.Add(Instantiate(Resources.Load("Prefabs/BlockA")) as GameObject);
+	}
+
+	public void CreateThreeBlocks(){
+		gameObjectList.Add(Instantiate(Resources.Load("Prefabs/BlockA")) as GameObject); 
+		gameObjectList.Add(Instantiate(Resources.Load("Prefabs/BlockA")) as GameObject); 
+		gameObjectList.Add(Instantiate(Resources.Load("Prefabs/BlockA")) as GameObject); 
+		float x = gameObjectList [gameObjectList.Count - 2].transform.position.x;
+		float y = gameObjectList [gameObjectList.Count - 2].transform.position.y;
+		gameObjectList [gameObjectList.Count - 1].transform.position = new Vector3 (x + 0.9f, y + 1.5f, 0);
 	}
 
 	public void PauzeBlocks(){
