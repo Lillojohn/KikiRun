c8c57c79085ca38f992c159c4894a9811d043d3e
Jonathan Emanuels <j.emanuels@outlook.com>
2017-06-19 01:20:32 +0200

    Jump indicator en bijhouden hoelang het mannetje stilstaat.

---
+++ Assets/JumpIndicator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpIndicator : MonoBehaviour {
+
+	public GameObject indicationCircle;
+
+	// Use this for initialization
+	void Start () {
+		indicationCircle = GameObject.Find ("IndicationCircle");
+		indicationCircle.GetComponent<SpriteRenderer> ().color = new Color (0, 0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D  col)
+	{
+		if(gameObject.name == "JumpIndicator1" && col.gameObject.name == "Nick")
+		{
+			indicationCircle.GetComponent<SpriteRenderer> ().color = new Color (1, 0, 0, 1);
+		}
+
+		if(gameObject.name == "JumpIndicator2" && col.gameObject.name == "Nick")
+		{
+			indicationCircle.GetComponent<SpriteRenderer> ().color = new Color (1, 0.9f, 0, 1);
+		}
+
+		if(gameObject.name == "jumpIndicator3" && col.gameObject.name == "Nick")
+		{
+			indicationCircle.GetComponent<SpriteRenderer> ().color = new Color (0, 1, 0, 1);
+		}
+	}  
+
+	void OnTriggerExit2D (Collider2D  col){
+		if(gameObject.name == "jumpIndicator3" && col.gameObject.name == "Nick")
+		{
+			indicationCircle.GetComponent<SpriteRenderer> ().color = new Color (0, 0, 0, 0);
+		}
+	}
+}
---
+++ Assets/StandingManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class StandingManager : MonoBehaviour {
+
+	private float standingTime;
+	private GameObject goal;
+
+	// Use this for initialization
+	void Start () {
+		standingTime = 0;
+		goal = GameObject.Find ("Goal");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameObject.GetComponent<PlatformerCharacter2D> ().moving) {
+			standingTime = Time.time;
+		}
+	}
+
+	public float GetStandingTime(){
+		return standingTime;
+	}
+
+
+}
